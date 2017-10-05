3346835a8fa6079c395713713baff0d981400b8d
MitchStark10 <MitchStark10@gmail.com>
2017-10-05 09:45:34 -0400

    comments for where we will begin parsing through the elements

--- web_scraper.py
+++ web_scraper.py
@@ -8,9 +8,13 @@ soup = BeautifulSoup(html_page, "html.parser")
 #Only search through the main statistics table, this query is more selective so we get fewer results
 tab_panel = soup.find("div", { "class" : "tab-panel clearfix active "})
 stats_box = tab_panel.find( "div", { "class" : "stats-box stats-box-alternate full clearfix"})
+
+#sort through the table
 table = stats_box.find("table")
 for element in table:
     href = element.find("a")
     #if href contains "players" it is a player stat table, need to parse the data
     if "players" in str(href):
         print(element)
+        print('---------------------')
+        #This is where we will send in the html elements to a Player constructor where the data will be parsed
