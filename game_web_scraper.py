00b41d834eeaecdc2d6f715d83a3aa5bbbdd88bd
MitchStark10 <MitchStark10@gmail.com>
2017-10-19 09:44:19 -0400

    adding additional teams to collect information from

--- game_web_scraper.py
+++ game_web_scraper.py
@@ -34,46 +34,51 @@ def collectPlayerGameData(soup, table_class_string):
 
 
 #schedule page
-calvin_schedule_page = 'http://calvinknights.com/sports/msoc/2017-18/schedule'
-html_schedule_page = urllib2.urlopen(calvin_schedule_page)
-schedule_soup = BeautifulSoup(html_schedule_page, "html.parser")
-game_links = schedule_soup.find_all("a", href=re.compile("boxscores"))
+calvin_base_page = 'http://calvinknights.com'
+kzoo_base_page = 'http://hornets.kzoo.edu'
+hope_base_page = 'http://athletics.hope.edu'
+website_list = [calvin_base_page, kzoo_base_page, hope_base_page]
 
 #list to keep track of games already collected
 #formatted "<home_team>-<away_team>"
 games_collected = []
 
+for site in website_list:
+    schedule_page = site + '/sports/msoc/2017-18/schedule'
+    html_schedule_page = urllib2.urlopen(schedule_page)
+    schedule_soup = BeautifulSoup(html_schedule_page, "html.parser")
+    game_links = schedule_soup.find_all("a", href=re.compile("boxscores"))
 
-for game_link in game_links:
-    calvin_game_page = 'http://calvinknights.com' + game_link['href']
-    html_page = urllib2.urlopen(calvin_game_page)
-    soup = BeautifulSoup(html_page, "html.parser")
+    for game_link in game_links:
+        game_page = site + game_link['href']
+        html_page = urllib2.urlopen(game_page)
+        soup = BeautifulSoup(html_page, "html.parser")
 
-    #This collects the scoring summaries
-    stats_box = soup.find( "div", { "class" : "stats-box half scoring-summary clearfix"})
-    if stats_box is None:
-        break
-    table = stats_box.find("tbody")
-    data = table.find_all("tr")
-    game_header = soup.find( "div", { "class" : "head"})
-    game_info = game_header.find("h1")
-    current_game = Game(game_info)
-    
-    if current_game.getGameString() in games_collected:
-        continue
-    
-    games_collected.append(current_game.getGameString())
-
-    for element in data:
-        current_game.addEvent(Event(element))
-
-    #This collects the starting lineup strings
-    logs = soup.findAll("tr", { "class" : "row" })
-    for log in logs:
-        if not checkForHalfTime(log):
+        #This collects the scoring summaries
+        stats_box = soup.find( "div", { "class" : "stats-box half scoring-summary clearfix"})
+        if stats_box is None:
+            break
+        table = stats_box.find("tbody")
+        data = table.find_all("tr")
+        game_header = soup.find( "div", { "class" : "head"})
+        game_info = game_header.find("h1")
+        current_game = Game(game_info)
+        
+        if current_game.getGameString() in games_collected:
             continue
-        print(log)
+        
+        games_collected.append(current_game.getGameString())
 
-    #This collects the game stats (goals and assists)
-    collectPlayerGameData(soup, "stats-box half lineup h clearfix")
-    collectPlayerGameData(soup, "stats-box half lineup v clearfix")
+        for element in data:
+            current_game.addEvent(Event(element))
+
+        #This collects the starting lineup strings
+        logs = soup.findAll("tr", { "class" : "row" })
+        for log in logs:
+            if not checkForHalfTime(log):
+                continue
+            print(log)
+
+        #This collects the game stats (goals and assists)
+        collectPlayerGameData(soup, "stats-box half lineup h clearfix")
+        collectPlayerGameData(soup, "stats-box half lineup v clearfix")
