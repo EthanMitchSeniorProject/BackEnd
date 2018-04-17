fcbe74f3b35bd7ec19a2e26023eb90a32ca93d42
MitchStark10 <mitch.stark.github@gmail.com>
2018-04-17 15:12:19 -0400

    update queries are now working

--- Soccer/soccer_player.py
+++ Soccer/soccer_player.py
@@ -151,7 +151,7 @@ class SoccerPlayer(object):
                 cursor = self._connection.cursor()
                 sql_command = "UPDATE player SET num = "+self._number+", year = '"+self._year+"', position = '"+self._position+"', games_played = "+str(self._games_played) \
                     +", games_started = "+str(self._games_started)+", points = "+str(self._points)+", shots = "+str(self._shots)+", shots_on_goal = " \
-                    +str(self._shots_on_goal)+", yellow_cards = "+str(self._yellow_cards)+", red_cards = "+str(self._red_cards)+", num = "+str(self._number)+" WHERE name = '"+self._name+"';"
+                    +str(self._shots_on_goal)+", yellow_cards = "+str(self._yellow_cards)+", red_cards = "+str(self._red_cards)+" WHERE name = '"+self._name+"';"
                 cursor.execute(sql_command)
                 self._connection.commit()
             else:
